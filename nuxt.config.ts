a00ad8c9cd13e6830a2c58a5f05858b1f71bc4dc
Nathan Fletcher <fletcher.nathan.a@gmail.com>
2025-06-03 23:22:35 +0000

    removing prerender images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,15 +2,6 @@
 import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   ssr: false,
-  nitro: {
-    prerender: {
-      routes: [
-        '/images/jt-hero-1.jpg',
-        '/images/jt-hero-2.jpg',
-        // etc.
-      ]
-    }
-  },
   devtools: { enabled: false },
   vite: {
     plugins: [
